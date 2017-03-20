59becdd2fb87550e358f6e9cd5467e63aa2253f2
Janet Walker <janetwalker@wifi_guest-10-38-108-191.uncc.edu>
2017-03-20 17:57:23 -0400

    Set up look and creating arrays for questions and answers

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+	var quoteArray = ["What was Norman Bate's hobby in Psycho?", 
+					  "Who was the first female monster to appear in a movie?",
+					  "In Hitchcock's Film, The Trouble with Harry, what was the trouble?"
+					  ];
+
+	var choiceArray = ["A. bird watching", "B. stuffing birds", "C. photography", "D. horse back riding"], 
+					  ["A. Medusa", "B. Gorgon", "C. Bride of Frankenstein", "D. Countess Marya Zaleska"],
+					  ["A. He was lazy", "B. He was depressed", "C. He lost his leg", "D. He was dead"];
+
+	var answerArray = ["B. stuffing birds", 
+					   "C. Bride of Frankenstein",
+					   "D. he was dead"
+					   ];
+
+	var counter = 0;
